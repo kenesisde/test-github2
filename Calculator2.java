bb954e1193638b3d0610b27c271e095a23c735a0
thewayifeel93 <thewayifeel93@gmail.com>
2017-08-08 15:49:31 +0900

    add minus() method

--- Calculator2.java
+++ Calculator2.java
@@ -4,9 +4,14 @@ public class Calculator{
 
   public static void main(String[] args){
         System.out.println( sum(a,b));
+        System.out.println( minus(a,b));
   }
 
   private static int sum(int a, int b){
     return a + b;
   }
+
+    private static int minus(int a, int b){
+      return a - b;
+    }
 }
