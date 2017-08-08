0f750bb97b18c10d9ec9098920eb07318b6bc79c
thewayifeel93 <thewayifeel93@gmail.com>
2017-08-08 15:48:16 +0900

    add sum() method

---
+++ Calculator2.java
@@ -0,0 +1,12 @@
+package com.mycompany.test;
+
+public class Calculator{
+
+  public static void main(String[] args){
+        System.out.println( sum(a,b));
+  }
+
+  private static int sum(int a, int b){
+    return a + b;
+  }
+}
